3ea34fb80511e007adf494d532f04b42e6b446ca
marti <marti@LAPTOP-HLE1905P>
2017-10-06 10:33:44 +0200

    Kommentarer i Command.java

--- src/semesterprojekt/Command.java
+++ src/semesterprojekt/Command.java
@@ -8,24 +8,26 @@ public class Command {
 
     private CommandWord commandWord;
     private String secondWord;
-
+    
+    //Laver et commandWord der skal have et secondWord
     public Command(CommandWord commandWord, String secondWord) {
         this.commandWord = commandWord;
         this.secondWord = secondWord;
     }
-
+    //Laver getter metoder
     public CommandWord getCommandWord() {
         return commandWord;
     }
-
+    
     public String getSecondWord() {
         return secondWord;
     }
-
+    
+    //Tjekker om den command der er blevet indtastet er korrekt
     public boolean isUnknown() {
         return (commandWord == CommandWord.UNKNOWN);
     }
-
+    //Tjekker om commandWord har second word
     public boolean hasSecondWord() {
         return (secondWord != null);
     }
