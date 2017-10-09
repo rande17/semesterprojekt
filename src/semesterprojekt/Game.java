e1d9290a85af72817532217c8431c5b03fc7f09b
frede <frede@DESKTOP-VO3C8E1>
2017-10-09 11:43:28 +0200

    Changed exit in Game.java

--- src/semesterprojekt/Game.java
+++ src/semesterprojekt/Game.java
@@ -44,7 +44,7 @@ public class Game {
         beach.setExit("seabottom", seaBottom);
         beach.setExit("camp", camp);
 
-        airport.setExit("west", beach);
+        airport.setExit("beach", beach);
        
 
 
