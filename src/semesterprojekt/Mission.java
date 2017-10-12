6cff3c60cff42cb8f840e63a360706bb52de1358
marti <marti@LAPTOP-HLE1905P>
2017-10-12 14:26:39 +0200

    Julie og jeg har oprettet metoder i Mission klassen

---
+++ src/semesterprojekt/Mission.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semesterprojekt;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author marti
+ */
+public class Mission {
+
+    HashMap<String, Boolean> missionStatus = new HashMap();
+    HashMap<String, String> missionInfo = new HashMap();
+    HashMap<String, Integer> missionPoint = new HashMap();
+
+    public void addMission(String name, String describtion, int point) {
+        missionInfo.put(name, describtion);
+        missionStatus.put(name, false);
+        missionPoint.put(name, point);
+    }
+
+    public String getMissionDescribtion(String key) {
+        return missionInfo.get(key);
+    }
+
+    public void setMissionComplete(String key) {
+        missionStatus.replace(key, false, true);
+        //Kalder ukendt klasse
+        Score.addToPoints(missionPoint.get(key));
+    }
+
+}
