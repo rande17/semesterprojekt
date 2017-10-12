aa101931221a4423cc6f94d3bef814456d9c20c1
Nicolai <Nicolai@MSI>
2017-10-12 16:10:15 +0200

    Added methods to the class Inventory - Nicolai

--- src/semesterprojekt/Inventory.java
+++ src/semesterprojekt/Inventory.java
@@ -5,10 +5,81 @@
  */
 package semesterprojekt;
 
+import java.util.*;
+
 /**
  *
  * @author marti
  */
 public class Inventory {
-    
+
+    private int inventoryMaxWeight = 10; //set default inventory weight 
+    private int currentInventoryWeight;
+    public Map<String, Integer> inventory = new HashMap<>(); //Create a HashMap
+//constucter  
+
+    Inventory() {
+    }
+//constructer
+
+    Inventory(int newInventoryMaxWeight) {
+        inventoryMaxWeight = newInventoryMaxWeight;
+    }
+//get max inventory weight
+
+    public int getInventoryMaxWeight() {
+        return inventoryMaxWeight;
+    }
+//get current inventory weight
+
+    public int getCurrentInventoryWeight() {
+        currentInventoryWeight = 0;
+        //Iterates through list of item in inventory to get current inventory weight 
+        for (Item item : int) {
+            currentWeight
+    }
+
+    {
+            currentInventoryWeight += item.weight
+        
+        
+    }
+
+        return currentInventoryWeight
+                = // accumulated weight of inventory?;
+    }
+
+//Set new weight for the inventory
+    void setInventoryMaxWeight(int newInventoryMaxWeight) {
+        inventoryMaxWeight = newInventoryMaxWeight;
+    }
+
+    useItem() {
+
+    }
+
+    public void showInventory() {
+        System.out.println(inventory + "\n");
+    }
+
+    public void addItemToInventory() {
+        if (checkInventoryMaxWeigth()) {
+            inventory.put(Item.Name, Item.Weight)   
+        } else {
+            System.out.println("Inventory is full");
+        }
+
+    }
+
+    dropItemInventory() {
+    }
+//Check max weigth of the inventory everytime you pick up a new item
+
+    public boolean checkInventoryMaxWeigth() {
+        if (currentInventoryWeight + item.getWeight <= inventoryMaxWeight) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
