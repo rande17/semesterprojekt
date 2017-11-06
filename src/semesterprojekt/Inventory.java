461ca2545fed92c295669260d4c5afa62f4bc9ce
finch <finch@DESKTOP-98PU31V>
2017-11-06 09:52:32 +0100

    Items class commented

--- src/semesterprojekt/Inventory.java
+++ src/semesterprojekt/Inventory.java
@@ -1,106 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package semesterprojekt;
 
 import java.util.*;
 
-/**
- *
- * @author marti
- */
 public class Inventory {
-    
+
+    // Data field
     private int inventoryMaxQuantity = 10;
     private int currentQuantity = 0;
     private int inventoryMaxWeight = 10; //set default inventory weight 
     private int currentInventoryWeight = 0;
     private HashMap<String, Integer> inventory = new HashMap<>(); //Create a HashMap
-//constuctor  
 
+    // Constructor, no arg  
     Inventory() {
     }
-//constructer
 
+    // Constructor with args
     Inventory(int newInventoryMaxWeight) {
         inventoryMaxWeight = newInventoryMaxWeight;
     }
-//get max inventory weight
 
+    // Method, getInventoryMaxWeight
     public int getInventoryMaxWeight() {
         return inventoryMaxWeight;
     }
 
-    public void addItemInInventory(Item _item) {
+    // Method, addItemToInventory
+    public void addItemToInventory(Item _item) {
         int quantity = 0;
         String itemInInventory = "";
         String itemName = _item.getName();
 
         if (currentQuantity < inventoryMaxQuantity) {
+            // If inventory already contains itemname, add 1 to quantity
             if (inventory.containsKey(itemName)) {
                 quantity = inventory.get(itemName) + 1;
 
             } else {
                 quantity = 1;
             }
+            // Adds item to inventory hashmap
             inventory.put(itemName, quantity);
             currentQuantity += 1;
         } else {
-            System.out.println("You can't pickup this item");
+            System.out.println("You cannot pickup this item");
         }
     }
 
-    // Drop item from inventory and add to Room 
+    // Method, drops item from inventory
     public void dropItemInventory(String _string) {
         int quantity = inventory.get(_string) - 1;
-
+        // If inventory quantity <= 1, remove item completely.
         if (inventory.get(_string) <= 1) {
             inventory.remove(_string);
 
+            // Else, decrease quantity of item in inventory by 1
         } else {
             inventory.replace(_string, quantity);
-
         }
-        
+
         currentQuantity = currentQuantity - 1;
 //        getItems itemList
 //        .put(_item, quantity);
 //        quantity = itemList.get(_item)
     }
 
-//get current inventory weight
+    // Method, get current inventory weight
     public int getCurrentInventoryWeight() {
         currentInventoryWeight = 0;
-        //Iterates through list of item in inventory to get current inventory weight 
 
+        //Iterates through list of item in inventory to get current inventory weight 
         for (String items : inventory.keySet()) {
             int quantity = inventory.get(items);
 //            int weight = items.getWeight();
             int weight = 1;
             int thisItemWeight = quantity * weight;
             currentInventoryWeight += thisItemWeight;
-
         }
-
         return currentInventoryWeight;
     }
 
-    // get inventory
+    // Method, get inventory
     public HashMap getInventory() {
         return inventory;
     }
 
-//Set new weight for the inventory
+    //Method, set new inventory max weight
     void setInventoryMaxWeight(int newInventoryMaxWeight) {
         inventoryMaxWeight = newInventoryMaxWeight;
     }
-
+    
+    // Method, use item
     public void useItem() {
 
     }
-
+    
+    // Method, show inventory
     public void showInventory() {
         System.out.println(inventory + "\n");
     }
